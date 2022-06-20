d937028126bd85cf229c1804f34fb334d3be2d67
Germanilu <lucianogermani93@gmail.com>
2022-06-20 13:31:06 +0200

    feat: refactor connection mongoose

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');// importo libreria de mongoose para que  funcione mongoose
+
+//connection to MONGODB
+const db = () => mongoose.connect(process.env.MONGO_URI) // aqui ejecuto la coneccion a mongoose, esto lo que me devuelve es una promesa y la uso con .then o .catch (/test es lo que voy a atacar en mi base de datos puedo cambiarla)
+.then(() => {
+    console.log('Connection stablished')
+})
+.catch((error) => {
+    console.log('Error connection to MongoDB',error)
+})
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express'); // almaceno en la variable express
-
-const mongoose = require('mongoose') // para q funcione mongoose
-
 require('dotenv').config(); // para q funcione dotenv
+const db = require('./config/database') // requiero el fichero de config/database.js donde tengo mongoose
+
+const app = express(); //ejecuto a expresss
 
 
-const app = express(); //llamo a express 
-
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; // Creo variable que va a usar mi servidor
 
 
 //routes
@@ -17,19 +14,16 @@ app.get('/' , (req,res) => {   // Primera ruta creada
 });
 
 
-//connection to MONGODB
-
-mongoose.connect(process.env.MONGO_URI) //esto lo que me devuelve es una promesa y la uso con .then o .catch (/test es lo que voy a atacar en mi base de datos puedo cambiarla)
-.then(() => {
-    console.log('Connection stablished')
-})
-.catch((error) => {
-    console.log('Error connection to MongoDB',error)
-})
-
-
+db().then(() => {// Ejecuto db para que funcione database.js
+//Levanto el sv ene l puerto indicado.
 app.listen(port, () => {   // como primer argumento le paso el puerto de la app
     console.log('server is running: ' + port);
 }) 
+}) 
+.catch((error) => {
+    console.log("error connecting to mongoDB ", error)
+})
+
+
 
 // (node index.js en terminal para comprobar que hasta aqui funciona)
